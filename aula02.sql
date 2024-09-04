e56b75ccb58fa04d3d791f9d7617aa4a30444350
marcospaulo27 <51270512+marcospaulo27@users.noreply.github.com>
2024-09-03 21:07:06 -0300

    Update aula02.sql

--- aula02.sql
+++ aula02.sql
@@ -1,6 +1,7 @@
+-- Criar banco de dados bd_sistema_bancario
 CREATE DATABASE bd_sistema_bancario;
 
--- Criar 5 tabelas: conta corrente, conta poupanca, empréstimo pessoal, previdência complementar e pagamento de contas
+-- Criar 5 tabelas quaisquer: conta corrente, conta poupanca, empréstimo pessoal, previdência complementar e pagamento de contas
 
 CREATE TABLE conta_corrente (
     id_conta SERIAL PRIMARY KEY,
@@ -36,43 +37,37 @@ CREATE TABLE pagamento_contas (
 
 -- Criar tabelas conforme MER (Sistema Bancario):
 
-CREATE TABLE Cliente (
+CREATE TABLE cliente (
     cliente_id SERIAL PRIMARY KEY,
     nome_cliente VARCHAR(100) NOT NULL,
     endereco_cliente TEXT,
     telefone_cliente VARCHAR(15)
 );
 
-CREATE TABLE Conta (
-    conta_id SERIAL PRIMARY KEY,
-    numero_conta VARCHAR(20) NOT NULL,
-    saldo NUMERIC(15, 2) NOT NULL,
-    cliente_id INT REFERENCES Cliente(cliente_id),
-    agencia_id INT REFERENCES Agencia(agencia_id)
-);
-
-CREATE TABLE Agencia (
+CREATE TABLE agencia (
     agencia_id SERIAL PRIMARY KEY,
     nome_agencia VARCHAR(100) NOT NULL,
     cidade_agencia VARCHAR(50)
 );
 
-CREATE TABLE Emprestimo (
+CREATE TABLE conta (
+    conta_id SERIAL PRIMARY KEY,
+    numero_conta VARCHAR(20) NOT NULL,
+    saldo NUMERIC(15, 2) NOT NULL,
+    cliente_id INT REFERENCES cliente(cliente_id),
+    agencia_id INT REFERENCES agencia(agencia_id)
+);
+
+
+CREATE TABLE emprestimo (
     emprestimo_id SERIAL PRIMARY KEY,
     numero_emprestimo VARCHAR(20) NOT NULL,
     valor NUMERIC(15, 2) NOT NULL,
     conta_id INT REFERENCES Conta(conta_id)
 );
 
-CREATE TABLE Transacao (
-    transacao_id SERIAL PRIMARY KEY,
-    tipo_transacao VARCHAR(50) NOT NULL,
-    valor NUMERIC(15, 2) NOT NULL,
-    data_transacao DATE NOT NULL,
-    conta_id INT REFERENCES Conta(conta_id)
-);
-
 -- Deletar as 5 tabelas criadas aleatoriamente 
+
 DROP TABLE conta_corrente;
 DROP TABLE conta_poupanca;
 DROP TABLE emprestimo_pessoal;
@@ -80,11 +75,16 @@ DROP TABLE previdencia_complementar;
 DROP TABLE pagamento_contas;
 
 -- alterar as tabelas conforme o MER atualizado
-ALTER TABLE Conta
-ADD COLUMN tipo_conta VARCHAR(50);
 
-ALTER TABLE Emprestimo
-ADD COLUMN taxa_juros NUMERIC(5, 2);
+ALTER TABLE cliente rename to tbl_cliente;
+ALTER TABLE conta rename to tbl_conta;
+ALTER TABLE emprestimo rename to tbl_emprestimo;
+ALTER TABLE agencia rename to tbl_agencia;
+
+ALTER TABLE tbl_cliente ADD COLUMN idade int;
+ALTER TABLE tbl_cliente ADD COLUMN cpf int;
 
 -- Criar um esquema com o seu nome
+
 CREATE SCHEMA Marcos;
+
